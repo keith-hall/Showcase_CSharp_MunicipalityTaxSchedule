d638761b60189cbe094d22c08f90487038ff995f
Keith Hall <kingkeith+github@gmail.com>
2017-06-03 15:10:34 +0300

    rename and organize files

--- MunicipalityTaxes/MunicipalityTaxes/Implementations/MunicipalityTaxesService.cs
+++ MunicipalityTaxes/MunicipalityTaxes/Implementations/MunicipalityTaxesService.cs
@@ -7,8 +7,7 @@ using System.Text;
 
 namespace MunicipalityTaxes
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
-    public class Service1 : IService1
+    public class MunicipalityTaxesService : IMunicipalityTaxesService
     {
         public string GetData (int value)
         {
--- MunicipalityTaxes/MunicipalityTaxes/Interfaces/IMunicipalityTaxesService.cs
+++ MunicipalityTaxes/MunicipalityTaxes/Interfaces/IMunicipalityTaxesService.cs
@@ -7,9 +7,8 @@ using System.Text;
 
 namespace MunicipalityTaxes
 {
-    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
     [ServiceContract]
-    public interface IService1
+    public interface IMunicipalityTaxesService
     {
         [OperationContract]
         string GetData (int value);
